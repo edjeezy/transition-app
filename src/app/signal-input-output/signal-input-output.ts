b7a55a5aa38d83428b598586ce0c9163df513602
Mouhamed Mar <mouhamededji@gmail.com>
2025-09-04 19:07:52 +0000

    new input syntax

--- src/app/signal-input-output/signal-input-output.ts
+++ src/app/signal-input-output/signal-input-output.ts
@@ -8,5 +8,15 @@ import { Component, input, Input } from '@angular/core';
 })
 export class SignalInputOutput {
   @Input() monnom!: string;
+  simpleInput = input('Simple');
   nom = input.required<string>();
+  // On accepte une chaîne ou un booléen, et on le transforme toujours en booléen.
+  // C'est très utile pour les attributs HTML qui n'ont pas de valeur (ex: <comp disabled>).
+  disabled = input(false, {
+    transform: (value: boolean | string) => {
+      // Si c'est une chaîne, on vérifie si elle est vide (attribut présent) ou égale à "true".
+      return typeof value === 'string' ? value === '' || value.toLowerCase() === 'true' : value;
+    }
+  });
+
 }
