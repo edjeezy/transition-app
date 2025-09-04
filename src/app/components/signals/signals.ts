1f4d31dcf78700a8d351c6bc28df464cdb2e6ab5
Mouhamed Mar <mouhamededji@gmail.com>
2025-09-04 18:48:57 +0000

    into to effects

--- src/app/components/signals/signals.ts
+++ src/app/components/signals/signals.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, computed, effect, OnInit, signal, untracked, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
@@ -6,12 +6,39 @@ import { Component, signal } from '@angular/core';
   templateUrl: './signals.html',
   styleUrl: './signals.css'
 })
-export class Signals {
+export class Signals implements OnInit, AfterViewInit {
   // Writable Signals
   count = signal(0);
   userName = signal('John Doe');
   userProfile = signal({ id: 1, theme: 'dark' });
   tasks = signal(['Tâche 1', 'Tâche 2']);
+  prenom = signal('Mouhamed');
+  nom = signal('Mar')
+
+  // Computed Signals
+  fullName = computed(() => `${this.prenom()} ${this.nom()}`);
+  isNameTooLong = computed(() => this.fullName().length > 20);
+
+  @ViewChild('var') var!: any;
+
+  constructor() {
+    effect(() => {
+      console.log(`Le nom d'utilisateur a changé : ${this.fullName()}`);
+      localStorage.setItem('userName', this.fullName());
+      untracked(() => {
+        console.log(`Message associé : ${this.nom()}`);
+      });
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.var);
+
+  }
 
   changeCount() {
     this.count.set(this.count() + 1)
